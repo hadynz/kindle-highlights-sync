83db4672e92e24f1a88b79d78a732cca672e9c71
Hady Osman <hady.osman@xero.com>
2020-04-13 21:46:55 +1200

    Interface api interactions

--- src/services/kindleHighlightsApi.ts
+++ src/services/kindleHighlightsApi.ts
@@ -1,18 +1,25 @@
 import { Book } from 'puppeteer-goodreads';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+
+import { BookCreatedResponse } from '../interfaces/BookCreatedResponse';
 
 class KindleHighlightsApi {
-  public async createBook(book: Book): Promise<any> {
-    console.log(`creating book ${book.title}`);
+  public async createBook(book: Book): Promise<BookCreatedResponse> {
     try {
-      const response = await axios.post(
+      const { data } = await axios.post(
         'http://localhost:5000/api/books',
         book,
       );
+
+      const response: BookCreatedResponse = {
+        bookId: data.bookId,
+      };
+
       return response;
-    } catch (err) {
-      console.error(err);
-      throw err;
+    } catch (ex) {
+      const error: AxiosError = ex;
+      console.error(error);
+      throw error;
     }
   }
 }
