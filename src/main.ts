82abfe71d93e9abf1b16d94d15e52505091f2210
Hady Osman <hady.osman@xero.com>
2020-04-19 02:39:18 +1200

    Sync based on Diff with Api server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { PuppeteerGoodreads } from 'puppeteer-goodreads';
+
 import { KindleHighlightsApi } from './services/kindleHighlightsApi';
+import reduceListForSync from './utils/reduceListForSync';
 
 // Read secure keys from .env
-// const results = require('dotenv').config();
-// const { GOODREADS_LOGIN: login, GOODREADS_PASSWORD: password } = results.parsed;
+const results = require('dotenv').config();
+const { GOODREADS_LOGIN: login, GOODREADS_PASSWORD: password } = results.parsed;
 
 (async (): Promise<void> => {
   const defaultOptions = { headless: true };
@@ -13,18 +15,29 @@ import { KindleHighlightsApi } from './services/kindleHighlightsApi';
 
   const api = new KindleHighlightsApi();
 
-  // await goodreads.signin(login, password);
+  await goodreads.signin(login, password);
 
-  // const books = await goodreads.getMyBooks();
+  // Latest list of GoodReadsBooks
+  const goodreadBooks = await goodreads.getMyBooks();
 
-  const latestBook = (await api.getBooks())[0];
-  console.log(latestBook);
+  // Latest list of ApiBooks. Interested in what's the last ApiBook saved.
+  const lastSyncedBook = (await api.getBooks())[0];
+  console.log('Last Synced Book:', lastSyncedBook);
+
+  const booksToSync = reduceListForSync(goodreadBooks, lastSyncedBook);
+  console.log(
+    `Books in Goodreads: ${goodreadBooks.length}. Books to sync: ${booksToSync.length}`,
+  );
 
   // Loop in reverse order since list of books scraped from Goodreads is ordered (desc)
-  // for (let i = books.length; i > 0; i--) {
-  //   const book = books[i - 1];
-  //   await api.createBook(book);
-  // }
+  for (let i = booksToSync.length - 1; i >= 0; i--) {
+    const book = booksToSync[i];
+    try {
+      await api.createBook(book);
+    } catch (err) {
+      console.error(`Couldn't sync book: ${book.title}. Error: ${err}`);
+    }
+  }
 
   await goodreads.close();
 })();
