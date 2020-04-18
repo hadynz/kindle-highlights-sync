b06b3081c2315fb4d66239654d6913e166a1d3ae
Hady Osman <hady.osman@xero.com>
2020-04-19 10:19:51 +1200

    Add syncbook method

--- src/service/kindleHighlightsSyncService.ts
+++ src/service/kindleHighlightsSyncService.ts
@@ -1,23 +1,31 @@
-import { PuppeteerGoodreads } from 'puppeteer-goodreads';
+import { PuppeteerGoodreads, Book as GoodreadsBook } from 'puppeteer-goodreads';
 
 import { KindleHighlightsApi } from '../repository/kindleHighlightsApi';
 import reduceListForSync from '../util/reduceListForSync';
 
 class KingleHighlightsSyncService {
   private _goodreadsScraper: PuppeteerGoodreads;
+  private _api: KindleHighlightsApi;
 
   constructor(goodreadsScraper: PuppeteerGoodreads) {
     this._goodreadsScraper = goodreadsScraper;
+    this._api = new KindleHighlightsApi();
+  }
+
+  public async syncBook(book: GoodreadsBook): Promise<void> {
+    try {
+      await this._api.createBook(book);
+    } catch (err) {
+      console.error(`Couldn't sync book: ${book.title}. Error: ${err}`);
+    }
   }
 
   public async sync(): Promise<void> {
-    const api = new KindleHighlightsApi();
-
-    // Latest list of GoodReadsBooks
+    // Scrape complete list of GoodReadsBooks
     const goodreadBooks = await this._goodreadsScraper.getMyBooks();
 
     // Latest list of ApiBooks. Interested in what's the last ApiBook saved.
-    const lastSyncedBook = (await api.getBooks())[0];
+    const lastSyncedBook = (await this._api.getBooks())[0];
     console.log('Last Synced Book:', lastSyncedBook);
 
     const booksToSync = reduceListForSync(goodreadBooks, lastSyncedBook);
@@ -28,11 +36,7 @@ class KingleHighlightsSyncService {
     // Loop in reverse order since list of books scraped from Goodreads is ordered (desc)
     for (let i = booksToSync.length - 1; i >= 0; i--) {
       const book = booksToSync[i];
-      try {
-        await api.createBook(book);
-      } catch (err) {
-        console.error(`Couldn't sync book: ${book.title}. Error: ${err}`);
-      }
+      await this.syncBook(book);
     }
   }
 }
